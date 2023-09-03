3624b710f91fc56347299569b1416d49ad32d002
小植哥 <55085568+makeevery@users.noreply.github.com>
2023-09-04 03:59:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'UrKxgNysSzwXZR5dMUVwp3zTcGFPAbsV8ukC8nQ9SmE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '11-1',
+      horoscopeDate: '11-01',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '臭宝', year: '1999', date: '11-1',
+          type: '生日', name: '臭宝', year: '1999', date: '11-01',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '02-11',
@@ -44,7 +44,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2023-02-11' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '20xx' },
+        { keyword: 'marry_day', date: '2022-02-11' },
       ],
     },
   ],
